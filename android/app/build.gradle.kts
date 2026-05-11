9dc4e8684b0004cf828d7777e24a250e8c3d042a
Muhammad Usama <198348821+usama-248@users.noreply.github.com>
2026-05-11 15:18:07 +0500

    Bundle id updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.FutureDialLabs.facebook.video.downloader"
+    namespace = "com.FutureLabszee.facebook.video.downloader"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.FutureDialLabs.facebook.video.downloader"
+        applicationId = "com.FutureLabszee.facebook.video.downloader"
         // FFmpeg Kit requires API 24+.
         minSdk = maxOf(flutter.minSdkVersion, 24)
         targetSdk = 36
